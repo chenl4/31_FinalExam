391e3169c402c2fc9483a67336060b546ba40a50
chenl4 <chenl4@rose-hulman.edu>
2018-05-24 14:17:34 -0400

    blah

--- src/problem5.py
+++ src/problem5.py
@@ -2,9 +2,9 @@
 Final exam, problem 5.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Lilin Chen.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -136,10 +136,18 @@ def problem5(m, numbers1, numbers2):
       :type [str]
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
+    stuff_to_return = []
+
+    for k in range(len(numbers1)):
+        if numbers1[k] * m == numbers2[k]:
+            stuff_to_return = stuff_to_return + [numbers1[k]] + [numbers2[k]]
+            return stuff_to_return
+
+    return []
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
